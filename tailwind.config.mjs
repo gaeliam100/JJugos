91b14e61cd664543f4050c4751156b501c537818
Gael Iván <gaelivan740@example.com>
2025-04-08 09:54:22 -0600

    lastchanges

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { heroui } = require("@heroui/react");
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
